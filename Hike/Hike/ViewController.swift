99192b816a0302951781f1cabaf71a4d05dbb056
Hike-app <68992515+Hike-app@users.noreply.github.com>
2020-07-30 12:27:14 +0300

    Add files via upload

---
+++ Hike/Hike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hike
+//
+//  Created by PS-hacker on 30/07/2020.
+//  Copyright © 2020 Alpamayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
